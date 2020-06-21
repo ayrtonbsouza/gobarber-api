d0369a73a006c0614e4cc8cc67b3a4308b767329
Ayrton Souza <ayrtonsouza@icloud.com>
2020-06-21 04:35:10 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🚀 Server started and running on http://localhost:3333/');
+});
