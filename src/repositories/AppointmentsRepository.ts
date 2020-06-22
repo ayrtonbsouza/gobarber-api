631468df0c7a08e53f7bc99db4f38d930ecb78ac
Ayrton Souza <ayrtonsouza@icloud.com>
2020-06-22 17:14:28 -0300

    DTO implementation

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -2,6 +2,15 @@ import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
 
 /**
+ * Data Transfer Object
+ */
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+/**
  * Persistência <-> Repository <-> Rota
  * Repositório: Aonde buscamos informações,find, create, etc.
  * Um repositório por Model.
@@ -18,8 +27,8 @@ class AppointmentsRepository {
     return this.appointments;
   }
 
-  public create(provider: string, date: Date): Appointment {
-    const appointment = new Appointment(provider, date);
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
     this.appointments.push(appointment);
     return appointment;
   }
