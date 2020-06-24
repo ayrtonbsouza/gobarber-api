39e7de4f133401b57e453f01a200c7758090de44
Ayrton Souza <ayrtonsouza@icloud.com>
2020-06-23 23:44:39 -0300

    create user route and service development

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -15,12 +15,12 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
  */
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
@@ -34,7 +34,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
