d63442bfbc3ba30e7e638417a683aeaf8be04335
Ayrton Souza <ayrtonsouza@icloud.com>
2020-06-24 16:21:01 -0300

    global error handling development

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,31 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './configs/upload.config';
+import AppError from './errors/AppError';
 import './database';
 
 const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    console.error(error);
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('🚀 Server started and running on http://localhost:3333/');
