fd58d25c410656408b9d60a79130227f4e6e5901
Ayrton Souza <ayrtonsouza@icloud.com>
2020-06-23 22:54:56 -0300

    models relationship

--- src/database/migrations/1592936000278-CreateUser.ts
+++ src/database/migrations/1592936000278-CreateUser.ts
@@ -8,7 +8,7 @@ export default class CreateUser1592936000278 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
