794ff20750a633a7d0c3d07b83fb5e1d81c88ae5
Ayrton Souza <ayrtonsouza@icloud.com>
2020-08-03 16:58:41 -0300

    adding user_id field to appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,6 +16,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '123123123',
       provider_id: '123123123',
     });
     await expect(appointment).toHaveProperty('id');
@@ -27,12 +28,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '123123123',
       provider_id: '123123123',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '123123123',
         provider_id: '123123123',
       }),
     ).rejects.toBeInstanceOf(AppError);
