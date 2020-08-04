761f90e3a7be14bc30bcc21a4a20c334e6d4a9b8
Ayrton Souza <ayrtonsouza@icloud.com>
2020-08-03 23:06:58 -0300

    finishing appointments services

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,25 @@
 import { Router } from 'express';
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
 
 export default providersRouter;
