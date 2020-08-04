510b5dbef0ebce6ce3e04ba2479564aef0cc29a8
Ayrton Souza <ayrtonsouza@icloud.com>
2020-08-04 11:30:10 -0300

    developing notification when a new appointment is booked

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -12,9 +12,12 @@ import UserTokensRepository from '@modules/users/infra/typeorm/repositories/User
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
-container.registerSingleton<IAppointmentsRepository>(
-  'AppointmentsRepository',
-  AppointmentsRepository,
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
 );
 
 container.registerSingleton<IUserTokensRepository>(
@@ -22,7 +25,12 @@ container.registerSingleton<IUserTokensRepository>(
   UserTokensRepository,
 );
 
-container.registerSingleton<IUsersRepository>(
-  'UsersRepository',
-  UsersRepository,
+container.registerSingleton<IAppointmentsRepository>(
+  'AppointmentsRepository',
+  AppointmentsRepository,
+);
+
+container.registerSingleton<INotificationsRepository>(
+  'NotificationsRepository',
+  NotificationsRepository,
 );
