93563ee5ce47f466700b568358cc1468c0874687
Ayrton Souza <ayrtonsouza@icloud.com>
2020-08-05 19:02:40 -0300

    config aaplication to make uploads to amazon s3

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -9,7 +9,7 @@ import UserAvatarController from '@modules/users/infra/http/controllers/UserAvat
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
