5913a5f5c86640ecfc25fda2a0bd62ad83230d7e
Ayrton Souza <ayrtonsouza@icloud.com>
2020-08-12 03:17:57 -0300

    bug fix

--- src/configs/upload.config.ts
+++ src/configs/upload.config.ts
@@ -12,7 +12,6 @@ interface IUploadConfig {
     storage: StorageEngine;
   };
   config: {
-    // eslint-disable-next-line @typescript-eslint/ban-types
     disk: {};
     aws: {
       bucket: string;
@@ -29,8 +28,8 @@ export default {
       destination: tmpFolder,
       filename(request, file, callback) {
         const fileHash = crypto.randomBytes(10).toString('hex');
-        const fileName = `${fileHash}-${file.originalname}`;
-        return callback(null, fileName);
+        const filename = `${fileHash}-${file.originalname}`;
+        return callback(null, filename);
       },
     }),
   },
