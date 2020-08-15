446d531f9d223142607f431e6b674e6b8260d0c7
Ayrton Souza <ayrtonsouza@icloud.com>
2020-08-15 02:04:10 -0300

    serializing cache providers list

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -25,7 +26,10 @@ class ListProvidersService {
       users = await this.usersRepository.findAllProviders({
         exceptUserId: user_id,
       });
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
     return users;
   }
